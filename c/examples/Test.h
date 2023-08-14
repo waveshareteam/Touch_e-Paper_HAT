32568ce58f772c64120cdac92c1b42d7bc761aad
shhds <1873145421@qq.com>
2023-08-14 10:52:31 +0800

    Added 2.13inch_V4 Touch e-paper HAT routine

--- c/examples/Test.h
+++ c/examples/Test.h
@@ -9,6 +9,7 @@
 
 int TestCode_2in13(void);
 int TestCode_2in13_V3(void);
+int TestCode_2in13_V4(void);
 int TestCode_2in9(void);
 
 #endif
--- c/examples/main.c
+++ c/examples/main.c
@@ -3,7 +3,8 @@
 int main()
 {
 	// TestCode_2in13();
-	TestCode_2in13_V3();
+	// TestCode_2in13_V3();
+	// TestCode_2in13_V4();
 	// TestCode_2in9();
 	return 0;
 }
